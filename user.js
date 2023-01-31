420ab8a8569907119411818e1e7bd8821951cbee
Elias0419 <108772953+Elias0419@users.noreply.github.com>
2023-01-30 22:24:19 -0500

    Create user.js

---
+++ user.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name         API TEST
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.prolific.co/submissions
+// @match        https://app.prolific.co/submissions/*
+// @match        https://app.prolific.co/st
+// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
+// @require      https://code.jquery.com/jquery-3.6.3.js
+// @require      https://code.jquery.com/ui/1.13.1/jquery-ui.min.js
+// @require      https://unpkg.com/dexie/dist/dexie.js
+// @require      https://unpkg.com/ag-grid-community@29.0.0/dist/ag-grid-community.min.js
+// @resource     https://cdn.jsdelivr.net/npm/ag-grid-community/styles/ag-grid.css
+// @resource     https://cdn.jsdelivr.net/npm/ag-grid-community/styles/ag-theme-apline.css
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=prolific.co
+// @grant        none
+// ==/UserScript==
+
+//let tokenFrame;
+document.arrive(".help-centre", {onceOnly: true}, function (){
+let t = document.getElementsByClassName("nav-link help-centre")[0];
+	if (t)
+	{let e = t.parentNode;
+	let o = document.createElement("button");
+    o.id = "startButton";
+	o.innerHTML = "Start";
+	e.insertBefore(o, t);
+document.getElementById("startButton").addEventListener("click", function() {
+    console.log("clicked start");
+    tokenFrame()
+.then(tokenLogic)
+
+
+       // .then(getIT)
+        .catch(function(error) {
+            console.error(error);
+        });
+});
+
+async function tokenFrame() {
+console.log("tokenFrame1");
+    //tokenLogic()
+    //return new Promise((resolve, reject) => {
+        let iframe = document.createElement("iframe");
+        iframe.src = "https://app.prolific.co/submissions/1";
+        iframe.style.width = "80px";
+        iframe.style.height = "240px";
+        iframe.id = "frame";
+        let summaryDiv = document.querySelector(".summary");
+        summaryDiv.appendChild(iframe);
+        console.log("tokenFrame2");
+
+        return(iframe);
+console.log("tokenFrame3");
+    };
+}
+
+
+
+ async function tokenLogic(iframe) {
+  console.log("tokenlogic1");
+ // const iframe = await tokenFrame();
+
+  return new Promise((resolve, reject) => {
+    console.log("tokenlogic2");
+
+    const authRequestMarker = 'openid-connect/token';
+    const tokenFieldName = 'authorization';
+    var authRequest = null;
+
+    var open = iframe.contentWindow.XMLHttpRequest.prototype.open;
+    iframe.contentWindow.XMLHttpRequest.prototype.open = function() {
+      this.onreadystatechange = () => {
+        authRequest = this;
+      };
+
+      return open.apply(this, [].slice.call(arguments));
+    };
+
+    var headers = iframe.contentWindow.XMLHttpRequest.prototype.setRequestHeader;
+    iframe.contentWindow.XMLHttpRequest.prototype.setRequestHeader = function() {
+      if (arguments[0] == 'Authorization') {
+        let token = arguments[1].substring(7);
+        console.log("tokenlogic3");
+        console.log(token);
+        resolve(token);
+        getIT(token);
+      }
+    };
+  });
+}})
+
+function getIT(token, iframe) {
+  console.log("fetch1");
+    const response = fetch(
+      "https://internal-api.prolific.co/api/v1/submissions/?participant=59bd5ee0b7a3f000017d5c60&page=2&page_size=100",
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          Accept: "application/json, text/plain,  */*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "en-US,en;q=0.9",
+          Referer: "https://app.prolific.co/",
+          Origin: "https://app.prolific.co"
+        }
+      }
+    );
+   response.then(res => res.json()).then(data => {
+  console.log(data);
+});
+  };
